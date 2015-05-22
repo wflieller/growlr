3295e4b74d3da19005d47f215212c334ae6a37a0
Walker Flieller <wflieller@gmail.com>
2015-05-22 11:58:35 -0500

    Committing changes chitter

--- app.rb
+++ app.rb
@@ -1,3 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+['/', '/home'].each do |path|
+get path do
+if session[:userid].nil? then
+erb :login
+else
+redirect "/#{User.get(session[:userid]).email}"
+end
+end
+end
+
